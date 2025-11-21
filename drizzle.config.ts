37a12037b5db0b01f966ed16b17b382cf6546bb8
Nickgreenjeans <132416769+Nickgreenjeans@users.noreply.github.com>
2025-11-20 23:54:44 -0600

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./shared/schema.ts",
+  schema: "./server/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
